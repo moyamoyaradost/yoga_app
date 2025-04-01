ffb8dc6b379460d31ff81dbf1f93440fb6a0f997
Max Novikhin <novihinm@yandex.ru>
2025-04-01 16:27:21 +0300

    Исправлен таймер, добавлен ведущий ноль

--- js/script.js
+++ js/script.js
@@ -23,6 +23,61 @@ window.addEventListener('DOMContentLoaded', function() {
 
     // Начальное состояние - показан первый таб, остальные скрыты
     hideTabContent(1);
+    // Timer 
+    let deadline = '2025-04-01T17:00:00'; // Будущее время
+
+
+function getTimeRemaining(endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date());
+    
+    // Обнуляем значения при отрицательном времени
+    if (t <= 0) {
+        return {
+            'total': 0,
+            'hours': 0,
+            'minutes': 0,
+            'seconds': 0
+        };
+    }
+    
+    return {
+        'total': t,
+        'hours': Math.floor(t / (1000 * 60 * 60)),
+        'minutes': Math.floor((t / (1000 * 60)) % 60),
+        'seconds': Math.floor((t / 1000) % 60)
+    };
+}
+
+function setClock(id, endtime) {
+    let timer = document.getElementById(id),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds');
+
+    // Функция добавления нуля
+    const addZero = num => num <= 9 ? `0${num}` : num;
+
+    function updateClock() {
+        let t = getTimeRemaining(endtime);
+
+        // Обновляем значения только если время не истекло
+        hours.textContent = addZero(t.hours);
+        minutes.textContent = addZero(t.minutes);
+        seconds.textContent = addZero(t.seconds);
+
+        // Останавливаем таймер при истечении времени
+        if (t.total <= 0) {
+            clearInterval(timeInterval);
+        }
+    }
+
+    // Первоначальный запуск
+    updateClock();
+    const timeInterval = setInterval(updateClock, 1000);
+}
+
+setClock('timer', deadline);
+
 
     // Обработчик клика по заголовкам табов
     info.addEventListener('click', function(event) {
